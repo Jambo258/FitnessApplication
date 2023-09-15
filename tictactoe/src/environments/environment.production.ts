b030dbc8db6a9eff9602a9a37bc9ddfd213942d5
Jambo258 <jambomyllymaa@gmail.com>
2023-09-15 11:28:08 +0300

    variables

---
+++ tictactoe/src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  backendUrlUsers: 'https://project-api-heeh.onrender.com/api/users',
+  backendUrlHealthStatus: 'https://project-api-heeh.onrender.com/api/health',
+  backendUrlTraining: 'https://project-api-heeh.onrender.com/api/training',
+};
---
+++ tictactoe/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  backendUrlUsers: 'http://localhost:8080/api/users',
+  backendUrlHealthStatus: 'http://localhost:8080/api/health',
+  backendUrlTraining: 'http://localhost:8080/api/training',
+};
